6a9d7033d62707259120b6a75a4b26eb9c9e86d8
awarmath <andrew@carmunity.io>
2020-10-24 11:26:02 -0500

    add questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,126 @@
-var timerEl = document.getElementById('countdown');
-var startBtn = document.getElementById('start');
+// Gathering HTML elements for manipulation
+var quizBody = document.getElementById("quiz");
+var resultsEl = document.getElementById("result");
+var finalScoreEl = document.getElementById("finalScore");
+var gameoverDiv = document.getElementById("gameover");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreDiv = document.getElementById("high-scorePage");
+var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var endGameBtns = document.getElementById("endGameBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("highscore-score");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
 
-// Timer that counts down from 60
-function countdown() {
-  var timeLeft = 60;
 
-  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function() {
-    if (timeLeft > 1) {
-      timerEl.textContent = timeLeft + " seconds remaining"
-      timeLeft--;
-    }
-    else if (timeLeft === 1) {
-      timerEl.textContent = `${timeLeft} second remaining.`
-      timeLeft--;
-    }
-    else {
-      timerEl.textContent = ""
-      clearInterval (timeInterval);
-      displayMessage();
-    }
-  } , 1000);
+//add array with 5 questions
+var Questions = [
+    {
+        question: "Inside which HTML element do we put the Javascript?",
+        choiceA: "scripting",
+        choiceB: "script",
+        choiceC: "javacript",
+        choiceD: "js",
+        correctAnswer: "B"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        choiceA: "function = myFunction()",
+        choiceB: "function:myFunction()",
+        choiceC: "function myFunction()",
+        choiceD: "myFunction function()",
+        correctAnswer: "C"
+    },
+    {
+        question: "How do you call a function?",
+        choiceA: "myFunction()",
+        choiceB: "call function myFunction()",
+        choiceC: "call myFunction()",
+        choiceD: "Call my function()",
+        correctAnswer: "A"
+    },
+    {
+        question: "How do you round the number 7.25 to the nearest integer?",
+        choiceA: "Math.rnd(7.25)",
+        choiceB: "Math.round(7.25)",
+        choiceC: "round(7.25)",
+        choiceD: "rnd(7.25)",
+        correctAnswer: "B"
+    },
+    {
+        question: "How do you declare a JavaScript variable?",
+        choiceA: "variable carName",
+        choiceB: "v carName",
+        choiceC: "var carName",
+        choiceD: "var (carName)",
+        correctAnswer: "C"
+    },
+];
+
+//variables needed
+var finalQuestionIndex = Questions.length;
+var currentQuestionIndex = 0;
+var timeLeft = 76;
+var timerInterval;
+var score = 0;
+var correct;
+
+
+// Cycle through the questions
+function generateQuestions(){
+    if (currentQuestionIndex === finalQuestionIndex){
+        return showScore();
+    } 
+    var currentQuestion = Questions[currentQuestionIndex];
+    questionsEl.innerHTML = "<h3>" + currentQuestion.question + "</h3>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;
+    buttonD.innerHTML = currentQuestion.choiceD;
+};
+
+// Start quiz and timer while hiding the opening html
+function startQuiz(){
+    startQuizDiv.style.display = "none";
+    generateQuestions();
+
+    //Timer
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        quizTimer.textContent = "Seconds left: " + timeLeft;
+    
+        if(timeLeft === 0) {
+          clearInterval(timerInterval);
+          showScore();
+        }
+      }, 1000);
+    quizBody.style.display = "block";
 }
 
-startBtn.onclick = countdown;
+// Check answer 
+function checkAnswer(answer){
+    correct = Questions[currentQuestionIndex].correctAnswer;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+        score++;
+        alert("That Is Correct!");
+        currentQuestionIndex++;
+        generateQuizQuestion();
+    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+        alert("That Is Incorrect.")
+        currentQuestionIndex++;
+        generateQuizQuestion();
+    }else{
+        showScore();
+    }
+}
+
+// This button starts the quiz!
+startQuizButton.addEventListener("click",startQuiz);
